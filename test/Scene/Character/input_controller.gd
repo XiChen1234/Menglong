3f7390934a460d76ed8994f2f57b26f332e9eeae
XiChen1234 <xichen888.private@gmail.com>
2026-02-26 14:53:32 +0800

    fix: rename move input
    将控制组件中存储的移动输入重命名，避免歧义

--- test/Scene/Character/input_controller.gd
+++ test/Scene/Character/input_controller.gd
@@ -4,4 +4,4 @@ class_name InputController
 
 func control() -> void:
 	var move_dir: Vector2 = Input.get_vector("left", "right", "up", "down")
-	set_move_dir(move_dir)
+	set_move_input(move_dir)
--- test/Script/Character/control_component.gd
+++ test/Script/Character/control_component.gd
@@ -7,25 +7,25 @@ class_name ControlComponent
 - 敌人：ai
 """
 
-## 移动方向
-var _move_dir: Vector2 = Vector2.ZERO
+## 移动输入
+var _move_input: Vector2 = Vector2.ZERO
 
 
-## 输入决策，子类需要继承重写
+## 输入决策，子类需要继承重写，实现具体的输入决策
 func control() -> void:
 	pass
 
 
-## 获取移动方向
+## 获取输入的移动方向
 func get_move_dir() -> Vector2:
-	return _move_dir
+	return _move_input
 
 
 ## 设置移动方向
-func set_move_dir(dir: Vector2) -> void:
-	_move_dir = dir
+func set_move_input(dir: Vector2) -> void:
+	_move_input = dir
 
 
 ## 获取移动状态
 func get_is_moving() -> bool:
-	return _move_dir.length_squared() > 0.0001
+	return _move_input.length_squared() > 0.0001
